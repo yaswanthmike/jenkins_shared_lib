444521b5befd112a2d1749b3a01b51c72b0bf1ea
yaswanthmike <155167153+yaswanthmike@users.noreply.github.com>
2025-05-15 16:19:00 +0530

    Update common_seed.groovy

--- jenkins/common_seed.groovy
+++ jenkins/common_seed.groovy
@@ -1,4 +1,4 @@
-def baseUrl = "https://github.com/ybmadhu/"
+def baseUrl = "https://github.com/yaswanthmike/"
 def repoName = "$reponame"
 def gitRepoUrl= baseUrl + repoName + '.git'
 def jobName = "$reponame"
